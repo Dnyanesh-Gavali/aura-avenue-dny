99f392cfe58d2ae5cff1cdc9cbdc4f82a880dc28
Dnyanesh Gavali <dnyaneshgavali07@gmail.com>
2026-08-08 18:10:07 +0530

    Update Nodemailer transporter configuration

--- server/src/config/mail.js
+++ server/src/config/mail.js
@@ -9,18 +9,14 @@ const nodemailer = require("nodemailer");
  */
 
 
-const transporter = nodemailer.createTransport({// Create a transporter object using Gmail service
-
-    service: "gmail",
-
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
     }
-
 });
 
 module.exports = transporter;
