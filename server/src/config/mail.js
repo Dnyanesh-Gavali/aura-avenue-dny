5bbddb77ce7fe14ba92cfa707c24d21c7cbd04e1
Dnyanesh Gavali <dnyaneshgavali07@gmail.com>
2026-08-08 18:20:09 +0530

    Force IPv4 in Nodemailer mail transporter
    Added family option to force IPv4 for Nodemailer transport.

--- server/src/config/mail.js
+++ server/src/config/mail.js
@@ -1,18 +1,10 @@
 const nodemailer = require("nodemailer");
 
-/**
- * 
- * this module sets up a mail transporter using Nodemailer with Gmail service. It uses environment variables for authentication credentials (EMAIL_USER and EMAIL_PASS). The transporter is exported for use in other parts of the application to send emails.
- * @module mail
- * @requires nodemailer
- * @returns {Object} transporter - Nodemailer transporter object for sending emails
- */
-
-
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
+    family: 4, // <-- This forces IPv4 (Bypasses Render's IPv6 block)
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
